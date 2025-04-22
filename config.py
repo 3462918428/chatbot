dc035feff562fff9eead0a6bb87868f7aec1717a
3462919428 <3462919428@qq.com>
2025-04-22 21:22:27 +0800

    初始提交

--- config.py
+++ config.py
@@ -3,28 +3,28 @@
 # 请替换为你的实际信息
 
 # 恋爱开始日期，格式：YYYY-MM-DD
-START_DATE = "2024-11-17"
+START_DATE = ""
 
 # 目标城市
-CITY = "承德"
+CITY = ""
 
 # 和风天气 API Key
 # 请前往 https://dev.qweather.com/ 注册获取你的API密钥
-WEATHER_API_KEY = "bc104e0bbbed477a8b7b06a8519607d2"
+WEATHER_API_KEY = ""
 
 # 微信公众号开发者凭证
 # 在微信公众平台 -> 开发 -> 基本配置 中获取
-APP_ID = "wx5b0c16f8e11f4482"
-APP_SECRET = "97c9ed20116a1acc198a3f23149bc13c"
+APP_ID = ""
+APP_SECRET = ""
 
 # 微信模板消息 ID
 # 在微信公众平台 -> 功能 -> 模板消息 中申请，审核通过后获得
-TEMPLATE_ID = "Vj5SRtyJ1-h-9khWhCYaVMaFqt-vircSn6-Z0FBc7d0"
+TEMPLATE_ID = ""
 
 # 接收消息的用户（女朋友）的微信 OpenID
 # 需要用户关注你的公众号，并在公众号后台 -> 用户管理 中查看其 OpenID
-USER_ID = "oainD6fPeYePFyYt_sgtQvDhynYI"
+USER_ID = ""
 
 # 生日配置（阳历和农历）
-BIRTHDAY_SOLAR = "05-24"  # 女朋友阳历生日：月-日
-BIRTHDAY_LUNAR = "06-16"  # 我的农历生日：月-日
+BIRTHDAY_SOLAR = ""  # 女朋友阳历生日：月-日
+BIRTHDAY_LUNAR = ""  # 我的农历生日：月-日
